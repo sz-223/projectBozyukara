1bc6d0b564429492e887c99b28e2e8eefdb3abbd
Glitch (glitch-hello-node) <none>
2021-09-26 13:15:23 +0000

    🐔🍘 Checkpoint ./server.js:36624942/250

--- server.js
+++ server.js
@@ -51,12 +51,12 @@ client.on('message', message =>{
  }
 });
 
-const mainChannelId = 863697257584656388;
+const mainChannelId = 863697257584656385;
 
 client.on('voiceStateUpdate', (oldGuildMember, newGuildMember) =>{
  if(oldGuildMember.voiceChannelID === undefined && newGuildMember.voiceChannelID !== undefined){
    if(client.channels.get(newGuildMember.voiceChannelID).members.size == 1){
-     if (newGuildMember.voiceChannelID == 725595164105768984) {
+     if (newGuildMember.voiceChannelID == 863697257584656391) {
        newGuildMember.voiceChannel.createInvite({"maxAge":"0"})
          .then(invite => sendMsg(
            mainChannelId, "<@" + newGuildMember.user.id +"> が通話を開始しました！\n" + invite.url
