4f83c6ca67bb9b55a3e789462c237e3ae8fa3a15
Glitch (glitch-hello-node) <none>
2021-11-29 15:36:28 +0000

    🎸👻 Checkpoint ./server.js:36624942/76

--- server.js
+++ server.js
@@ -84,11 +84,15 @@ async function userIconsVoiceCh(voiceCh){
       })
     )
     const posx = 29 * i;
-    ctx.drawImage(pfp, 0, 0, 128, 128, posx, 0, posx + 24, 24);
+    ctx.drawImage(pfp, 0, 0, 128, 128, posx, 0, 24, 24);
   }
   //const attachment = new MessageAttachment(canvas.toBuffer());
   //console.log(attachment.height);
   return canvas.toBuffer();
+}
+
+function drawAvatarHorizontal(ctx, order){
+  
 }
 
 if(!process.env.DISCORD_BOT_TOKEN){
